d316e4b2590ab0147a5c8c050d8cc7c9f33495b8
Katie H <katie@honsingerfamily.com>
2017-02-15 21:33:13 -0800

    lecture 8 and problem set 4

---
+++ lecture8/simple_divide.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 14 11:38:35 2017
+
+@author: katiebug
+"""
+
+def fancy_divide(list_of_numbers, index):
+    denom = list_of_numbers[index]
+   
+    return [simple_divide(item, denom) for item in list_of_numbers]
+
+def simple_divide(item, denom):
+    try:
+        return item / denom
+    
+    except ZeroDivisionError:
+        return 0
+        
+print(fancy_divide([0, 2, 4], 0))
